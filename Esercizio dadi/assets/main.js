eeeaaaaae2d4e8d184e48c7c6991a1c260052bac
Alek90 <4lek.90@gmail.com>
2021-10-14 17:31:42 +0200

    Impostato html.
    Impostati degli elementi utili in html (titolo facoltativo). Aggiunti paddaggi nei commenti in js.

--- Esercizio dadi/assets/main.js
+++ Esercizio dadi/assets/main.js
@@ -5,9 +5,15 @@ Stabilire il vincitore sulla base del risultato più alto. */
 
 // Variabili
 
+const gambleBtnElmt = document.getElementById("gamble");
+const result = document.getElementById("result");
 
-// Quando inizia l'azione?
+// Quando inizia l'azione?  Crea bottone per attivare.
 
 // Attivando l'azione si generano 2 numeri random da 1 a 6
 
+
+
 // Costituisco le condizioni che determinano il vincitore.
+
+// Restituisco un messaggio con il risultato.
