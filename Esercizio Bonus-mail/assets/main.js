d5affde36d3146f31ab1dfbdb18fcdf60de386ba
Alek90 <4lek.90@gmail.com>
2021-10-15 00:32:18 +0200

    Bonus completato.
    Esercizio completato rispettando la richiesta, risulta funzionante.

--- Esercizio Bonus-mail/assets/main.js
+++ Esercizio Bonus-mail/assets/main.js
@@ -1,15 +1,18 @@
 /*  Chiedere la email dell'utente, confrontarla con quelle ammesse, e mandare a schermo un messaggio relativo all'esito del confronto. */
 
+/* Alternativa Bonus - l'email utente deve essere inserita a schermo in un input. Dopodichè andrà avviato il confronto per mezzo del click su un bottone. Rimane dover mettere a schermo l'esito */
 
 
-// Utilizzare un prompt per chiedere la email all'utente.
+// Selezionare elementi della DOM
 
-const user_email = prompt ("Qual'è la tua email?");
-// console.log(user_email);
+const userEmailInputElement = document.getElementById("user_email");
+const check = document.getElementById("check");
+const result = document.getElementById("result");
+
 
 // Variabili utili
 let message;
-const result = document.getElementById("result")
+
 
 // Creare un array (?) contenente una serie di email "ammesse" (utilizziamo razze umane Skyrim).
 
@@ -19,17 +22,30 @@ const admitted = [
     "bretone@skyrim.tm", 
     "imperiale@skyrim.tm"
 ];
-// console.log(admitted);
 
-// Confrontare la email dell'utente con quelle presenti nell'array (array.includes costituisce confronto con risultato true/false).
 
-if (admitted.includes(user_email) == true) { // Se il risultato del confronto è true, creo un messaggio coerente e comprensibile
-    message = "email ammessa"
-}else { // Altrimenti (se il risultato è false/non è true), creo un messaggio coerente e comprensibile
-    message = "email rifiutata"
-}
-// console.log(message);
 
-// Mettere a schermo un messaggio relativo all'esito.
+/* Creare una funzione legata ad un bottone per:
+    Confrontare la email dell'utente con quelle presenti nell'array (array.includes costituisce confronto con risultato true/false).
+*/     
 
-result.innerHTML = message; 
+check.addEventListener("click", function() {
+
+    const user_email = userEmailInputElement.value;
+
+    if (admitted.includes(user_email) == true) { 
+        // Se il   risultato del confronto è true, creo un messaggio coerente e comprensibile
+        message = "email ammessa"
+    }else { 
+        // Altrimenti (se il risultato è false/non è true), creo un messaggio coerente e comprensibile
+        message = "email rifiutata"
+    }
+    
+    // Mettere a schermo un messaggio relativo all'esito.
+    
+    result.innerHTML = message; 
+})    
+
+
+
+
