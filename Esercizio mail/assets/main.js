e70a2361c7bea6fc2f21e59ce7ea391ef90a89ad
Alek90 <4lek.90@gmail.com>
2021-10-14 18:12:49 +0200

    Scomposti passaggi richiesti.

--- Esercizio mail/assets/main.js
+++ Esercizio mail/assets/main.js
@@ -1,3 +1,12 @@
 /*  Chiedere la email dell'utente, confrontarla con quelle ammesse, e mandare a schermo un messaggio relativo all'esito del confronto. */
 
 /* Aggiungere bonus - l'email utente deve essere mandata a schermo in un input. Dopodichè andrà avviato il confronto per mezzo del click su un bottone. Rimane dover mettere a schermo l'esito */
+
+
+// Utilizzare un prompt per chiedere la email all'utente.
+
+// Creare un array (?) contenente una serie di email "ammesse".
+
+// Confrontare la email dell'utente con quelle presenti nell'array (è stato accennato nella lezione).
+
+// Mettere a schermo un messaggio relativo all'esito.
