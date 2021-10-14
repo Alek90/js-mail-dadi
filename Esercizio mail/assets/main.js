c14358e41c99d80c9c7311ee75c32dc557b0b7fa
Alek90 <4lek.90@gmail.com>
2021-10-14 18:08:55 +0200

    Avviato esercizio email.
    Aperti file, impostata pagina html base, inserito commento descrittivo in js.

---
+++ Esercizio mail/assets/main.js
@@ -0,0 +1,3 @@
+/*  Chiedere la email dell'utente, confrontarla con quelle ammesse, e mandare a schermo un messaggio relativo all'esito del confronto. */
+
+/* Aggiungere bonus - l'email utente deve essere mandata a schermo in un input. Dopodichè andrà avviato il confronto per mezzo del click su un bottone. Rimane dover mettere a schermo l'esito */
