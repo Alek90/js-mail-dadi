4f3075762c08c24f3ffdbeda258395de10535470
Alek90 <4lek.90@gmail.com>
2021-10-14 17:25:17 +0200

    Creazione repo.
    Creata repo con le cartelle relative agli esercizi post visualizzazione della lezione persa.  Avviato es. dadi.

---
+++ Esercizio dadi/assets/main.js
@@ -0,0 +1,13 @@
+/* Generare numero random sia per il giocatore che per il computer.
+Stabilire il vincitore sulla base del risultato più alto. */
+
+
+
+// Variabili
+
+
+// Quando inizia l'azione?
+
+// Attivando l'azione si generano 2 numeri random da 1 a 6
+
+// Costituisco le condizioni che determinano il vincitore.
