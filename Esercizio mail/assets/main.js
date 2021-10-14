af57b03edc750c4ec2f6c68006e0f258c2bf3b34
Alek90 <4lek.90@gmail.com>
2021-10-14 19:17:39 +0200

    Inseriti alcuni passaggi dell'esercizio base.
    Inseriti i passaggi dell'esercizio base dopo aver eseguito un ripasso. Il bonus verrà separato per sostituire il riempimento del prompt con quello dell'input.

--- Esercizio mail/assets/main.js
+++ Esercizio mail/assets/main.js
@@ -5,13 +5,37 @@
 
 // Utilizzare un prompt per chiedere la email all'utente.
 
+const user_email = prompt ("Qual'è la tua email?");
+console.log(user_email);
+
+let message;
+
 // Mettere a schermo in un input la email.
 
-// Creare un array (?) contenente una serie di email "ammesse".
+// Creare un array (?) contenente una serie di email "ammesse" (utilizziamo razze umane Skyrim).
+
+const admitted = [ 
+    "nord@skyrim.tm", 
+    "redgard@skyrim.tm", 
+    "bretone@skyrim.tm", 
+    "imperiale@skyrim.tm"
+];
+console.log(admitted);
+
+// Confrontare la email dell'utente con quelle presenti nell'array (è stato accennato nella lezione).
+
+if (admitted.includes(user_email) == true) {
+    message = "email ammessa"
+}else {
+    message = "email rifiutata"
+}
+console.log(message);
 
 /*Creare una funzione legata ad un bottone per:
 
     Confrontare la email dell'utente con quelle presenti nell'array (è stato accennato nella lezione).
+
+
 
     Mettere a schermo un messaggio relativo all'esito.
  */
